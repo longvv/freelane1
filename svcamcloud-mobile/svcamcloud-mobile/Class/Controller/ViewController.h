0013b9d9624c364bad16dd31a39605ffd4fde1fb
Longvv <vvlong90@gmail.com>
2015-02-07 23:06:59 +0700

    import project
    import project

---
+++ svcamcloud-mobile/svcamcloud-mobile/Class/Controller/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  svcamcloud-mobile
+//
+//  Created by vu van long on 2/7/15.
+//  Copyright (c) 2015 FreelancerTeam. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
